f7ed6411022a646987128d323dac9c3fcb446e57
Marvash <marcusmerlin@gmail.com>
2018-01-25 11:25:14 +0100

    completed area function and added test file

--- area.js
+++ area.js
@@ -2,7 +2,7 @@
 module.exports = {
 	getArea:function (array)
 	{
-		if(Number.isInteger(array[0]) && Number.isInteger(array[1])
+		if(array != undefined && (array[0] != undefined && Number.isInteger(array[0]) && array[0] >= 0) && (array[1] != undefined && Number.isInteger(array[1]) && array[1] >= 0))
 		{
 			return array[0] * array[1];
 		}
